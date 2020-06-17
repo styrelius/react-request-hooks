65fc0acc633e7bd8c06ae33d16351bb5ed2bf6d0
Ellen Styrélius <ellen.styrelius@gmail.com>
2020-06-17 15:09:27 +0200

    Change name of type alias for return value in use-query.ts. Also add type alias 'Data'.

--- src/use-query.ts
+++ src/use-query.ts
@@ -1,18 +1,20 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-type UseQueryReturnValue = {
+type Data = any;
+
+type ReturnValue = {
   isLoading: boolean;
   isSuccessful: boolean;
   hasError: boolean;
-  data: any;
+  data: Data;
 };
 
-export const useQuery = (url: string): UseQueryReturnValue => {
+export const useQuery = (url: string): ReturnValue => {
   const [isLoading, setIsLoading] = useState(true);
   const [isSuccessful, setIsSuccessful] = useState(false);
   const [hasError, setHasError] = useState(false);
-  const [data, setData] = useState<any>(null);
+  const [data, setData] = useState<Data>(null);
 
   useEffect(() => {
     const source = axios.CancelToken.source();
