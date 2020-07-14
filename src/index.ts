dd85f278984761dc4ee584227a556a5a62dea0b3
Ellen Styrélius <ellen.styrelius@gmail.com>
2020-07-14 15:24:41 +0200

    Add use-mutation.ts with a hook to perform mutations, i.e. POST, PUT or DELETE requests (working only with the most basic HTTP request methods for now). Export it from index.ts.

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export { useQuery } from './use-query';
 export { useQueryLater } from './use-query-later';
+export { useMutation } from './use-mutation';
 
 axios.defaults.withCredentials = true;
